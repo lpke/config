4f9b7255dd4bcebc6f5b4c789d3fc6f1417de456
Luke <43902253+lpke@users.noreply.github.com>
2023-10-03 23:37:39 +1100

    Update keymaps.lua

--- nvim/lua/lpke/keymaps.lua
+++ nvim/lua/lpke/keymaps.lua
@@ -1,8 +1,9 @@
 local helpers = require('lpke.helpers')
 local keymap_set = helpers.keymap_set
--- SETTER SYNTAX: {'<modes><N=noremap,E=expr,!=silent>', <from>, <to>, {opts}}
+-- SETTER SYNTAX: {'<modes><R=recursive,E=expr,!=silent>', <from>, <to>, {opts}}
 
 -- remap leader to space
+keymap_set({'n', ' ', ''})
 vim.g.mapleader = ' '
 
 local keymaps = {
@@ -16,12 +17,16 @@ local keymaps = {
   -- TODO
 
   -- arrow-key scrolling
-  {'nvN', '<Down>', '<C-e>'},
-  {'nvN', '<Up>', '<C-y>'},
-  {'nvN', '<S-Down>', '4<C-e>'},
-  {'nvN', '<S-Up>', '4<C-y>'},
-  {'nvN', '<Left>', '6zh'},
-  {'nvN', '<Right>', '6zl'},
+  {'nv', '<Down>', '4<C-e>'},
+  {'nv', '<Up>', '4<C-y>'},
+  {'nv', '<S-Down>', '<C-e>'},
+  {'nv', '<S-Up>', '<C-y>'},
+  {'nv', '<Left>', '6zh'},
+  {'nv', '<Right>', '6zl'},
+
+  -- center cursor when down/up page
+  {'n', '<C-d>', '<C-d>zz'},
+  {'n', '<C-u>', '<C-u>zz'},
 
   -- horizontal mouse scrolling
   {'n', '<A-ScrollWheelDown>', '6zl'},
@@ -32,19 +37,27 @@ local keymaps = {
   {'v', 'K', ":m '<-2<CR>gv=gv"},
 
   -- wrapped line traversing
-  {'nNE', 'j', 'v:count ? "j" : "gj"'},
-  {'nNE', 'k', 'v:count ? "k" : "gk"'},
+  {'nE', 'j', 'v:count ? "j" : "gj"'},
+  {'nE', 'k', 'v:count ? "k" : "gk"'},
+
+  -- keep current reg when pasting over selected text
+  {'v', 'p', 'P'}, 
+  {'v', 'P', 'p'},
 
   {'n', 'J', 'mzJ`z'}, -- dont move cursor when joining lines
   {'n', '<leader>pv', vim.cmd.Ex}, -- open netrw
   {'n', '<leader>L', '<cmd>Lazy<cr>'}, -- package manager
-  {'nN!', '<C-w>w', ':set wrap!<CR>'}, -- toggle line wrap
+  {'n!', '<C-w>w', ':set wrap!<CR>'}, -- toggle line wrap
 }
 helpers.keymap_set_multi(keymaps)
 
 -- convert windows line endings to unix when pasting from global registers
 if helpers.is_wsl then
-  keymap_set({'nN!', '"*p', function() helpers.paste_unix('*') end})
-  keymap_set({'nN!', '"+p', function() helpers.paste_unix('+') end})
+  keymap_set({'n!', '"*p', function() helpers.paste_unix('*') end})
+  keymap_set({'n!', '"+p', function() helpers.paste_unix('+') end})
 end
 
+-- disabling shortcuts of :read to prevent accidental activation when typing :reg
+vim.cmd('cabbrev r echo "shorthand for :read disabled"')
+vim.cmd('cabbrev re echo "shorthand for :read disabled"')
+vim.cmd('cabbrev rea echo "shorthand for :read disabled"')
